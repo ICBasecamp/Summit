f72aec371ff818ab8315c30bc83262a1cb5d4398
fehervaryy <martindmhe@gmail.com>
2025-01-02 00:28:24 -0500

    text on side

--- ada-frontend-next/src/app/components/ticker-bar.tsx
+++ ada-frontend-next/src/app/components/ticker-bar.tsx
@@ -202,7 +202,7 @@ export function PlaceholdersAndVanishInput({
         onChange={handleChange}
         onKeyDown={handleKeyDown}
         ref={inputRef}
-        value={currentValue || value}
+        value={currentValue}
         type="text"
         className={cn(
           "w-full relative text-sm sm:text-base z-50 border-none dark:text-white bg-transparent text-white h-full rounded-full focus:outline-none focus:ring-0 pl-4 sm:pl-10 pr-20",
--- ada-frontend-next/src/app/pages/TickerForm.tsx
+++ ada-frontend-next/src/app/pages/TickerForm.tsx
@@ -6,8 +6,12 @@ import { PlaceholdersAndVanishInput } from '../components/ticker-bar';
 
 import { FlipWords } from "../components/flip-words";
 import { poppins } from '../layout';
+import { openSans } from '../layout';
 
 import Image from 'next/image';
+import { div } from 'framer-motion/client';
+
+import News from '../../../public/news.svg';
 
 interface TickerFormProps {
   setTicker: (ticker: string) => void;
@@ -102,37 +106,76 @@ const TickerForm: React.FC<TickerFormProps> = ({ setTicker }) => {
   };
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center px-4 self-center">
-      <div className="flex flex-col gap-4 mb-10 sm:mb-20 ">
-        <h1 className={`text-2xl text-left sm:text-5xl dark:text-white text-white ${poppins.className}`}>
-          Thinking of investing in <span className="inline-block w-[120px]"><FlipWords className="text-white" words={flipWords} duration={3000} /></span> <br />
-        </h1>
-        <p className={`text-2xl text-left sm:text-5xl dark:text-white text-white ${poppins.className}`}>We'll help you decide.</p>
-      </div>
+    <div className="h-screen flex gap-2 w-screen">
+      <div className="h-full w-2/3 flex flex-col justify-center items-center px-4 self-center">
+        <div className="flex flex-col gap-4 mb-10 sm:mb-20 ">
+          <h1 className={`text-2xl text-left sm:text-5xl dark:text-white text-white ${poppins.className}`}>
+            Thinking of investing in <span className="inline-block w-[120px]"><FlipWords className="text-white" words={flipWords} duration={3000} /></span> <br />
+          </h1>
+          <p className={`text-2xl text-left sm:text-5xl dark:text-white text-white ${poppins.className}`}>We'll help you decide.</p>
+        </div>
 
-      <div className="relative w-full max-w-xl ">
-        <PlaceholdersAndVanishInput
-          placeholders={placeholders}
-          onChange={handleChange}
-          onSubmit={handleSubmit}
-          currentValue={tickerInput}
-        />
-        {suggestions.length > 0 && (
-          <ul className="py-2 flex flex-col absolute bg-neutral-800 border border-zinc-900 w-full mt-1 rounded-lg shadow-lg z-10 max-h-48 overflow-y-auto">
-            {suggestions.map((suggestion, index) => (
-              <li
-                key={index}
-                className="px-4 py-3 cursor-pointer hover:bg-neutral-700 flex justify-between items-center"
-                onClick={() => handleSuggestionClick(suggestion.symbol)}
-              >
-                {suggestion.symbol} - {suggestion.fullName}
-                <span><Image src={suggestion.icon} alt={suggestion.fullName} width={28} height={28} className='rounded-md' /></span>
-              </li>
-            ))}
-          </ul>
-        )}
+        <div className="relative w-full max-w-xl ">
+          <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            currentValue={tickerInput}
+          />
+          {suggestions.length > 0 && (
+            <ul className="py-2 flex flex-col absolute bg-neutral-800 border border-zinc-900 w-full mt-1 rounded-lg shadow-lg z-10 max-h-48 overflow-y-auto">
+              {suggestions.map((suggestion, index) => (
+                <li
+                  key={index}
+                  className="px-4 py-3 cursor-pointer hover:bg-neutral-700 flex justify-between items-center"
+                  onClick={() => handleSuggestionClick(suggestion.symbol)}
+                >
+                  {suggestion.symbol} - {suggestion.fullName}
+                  <span><Image src={suggestion.icon} alt={suggestion.fullName} width={28} height={28} className='rounded-md' /></span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className={`h-full w-1/3 flex flex-col py-24 px-16 items-center justify-center ${openSans.className}`}>
+        {/* <div className="h-[1px] w-1/2 rounded-full bg-neutral-600"></div> */}
+        <div className='flex flex-col gap-12 items-start'>
+          <p className="text-2xl font-medium">Research instantly...</p>
+
+          <div className="flex flex-col gap-2 items-start">
+            <div className="flex gap-2 items-center">
+              {/* <img src="/news.svg" className="w-6 h-6 filter invert"/> */}
+              <p className="text-white text-xl">News</p>
+            </div>
+            <p className="font-light">See what news outlets are saying</p>
+          </div>
+          <div className="flex flex-col gap-2 items-start">
+            <div className="flex gap-2 items-center">
+              {/* <img src="/news.svg" className="w-8 h-8 filter invert"/> */}
+              <p className="text-white text-xl">Social Media</p>
+            </div>
+            <p className="font-light">See what everyone is saying</p>
+          </div>
+            <div className="flex flex-col gap-2 items-start">
+            <div className="flex gap-2 items-center">
+              {/* <img src="/news.svg" className="w-8 h-8 filter invert"/> */}
+              <p className="text-white text-xl">Economic Indicators</p>
+            </div>
+            <p className="font-light">Track market-moving economic data</p>
+            </div>
+            <div className="flex flex-col gap-2 items-start">
+            <div className="flex gap-2 items-center">
+              {/* <img src="/news.svg" className="w-8 h-8 filter invert"/> */}
+              <p className="text-white text-xl">Earnings Reports</p>
+            </div>
+            <p className="font-light">Review financial performance and forecasts</p>
+          </div>
+        </div>
       </div>
     </div>
+
+    
   );
 };
 
